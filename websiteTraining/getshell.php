00da8f8c91adcf0d95e5746198e4f468c3a95987
A-mIn3 <jerwhite0@gmail.com>
2017-05-20 18:12:52 +0200

    Create getshell.php

---
+++ websiteTraining/getshell.php
@@ -0,0 +1,18 @@
+<?php
+
+  /*
+       Goal : Get your web shell on the server. 
+  */
+
+        if(isset($_COOKIE["shell"])) {
+                
+                $key = base64_decode($_COOKIE["shell"]);
+                
+                if($key === "givemeshell"){
+                        echo $_SERVER['HTTP_USER_AGENT'];                       
+                        system("echo ".$_SERVER['HTTP_USER_AGENT']." >> /tmp/visiters.txt");
+                }
+        }       
+        
+
+?>
